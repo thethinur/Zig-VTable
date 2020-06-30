b5861fcbc2a0ec00ec88d3b7e316665018275913
thethinur <thinurtorsson@gmail.com>
2020-06-30 14:07:33 +0200

    Clean up names

--- vtable.zig
+++ vtable.zig
@@ -15,20 +15,20 @@ pub fn main() !void {
 }
 
 const Animal = struct {
-    class: ClassesEnum,
+    class: VTable.Tag,
     weight: i32,
 
-    const talk = ClassVTable.Fn("talk");
-    const weightG = ClassVTable.Fn("weightG");
+    const talk = VTable.Fn("talk");
+    const weightG = VTable.Fn("weightG");
 
-    const ClassVTable = VTable(ClassesEnum, Classes);
+    const VTable = CreateVTable(Tag, Functions);
     
-    const ClassesEnum = enum {
+    const Tag = enum {
         Dog,
         Cat,
         Frog
     };
-    const Classes = .{
+    const Functions = .{
         .Dog = struct {
             fn talk(self: *Animal) []const u8 {
                 return "Woof";
@@ -58,9 +58,9 @@ const Animal = struct {
 
 
 
-fn VTable (comptime ClassesEnum: type, comptime Classes: var) type {
-    const enumInfo = @typeInfo(ClassesEnum);
-    const classesInfo = @typeInfo(@TypeOf(Classes));
+fn CreateVTable (comptime TagType: type, comptime Map: var) type {
+    const enumInfo = @typeInfo(TagType);
+    const classesInfo = @typeInfo(@TypeOf(Map));
     
     if (enumInfo != .Enum)  @compileError("Enum is not an enum type");
     switch (classesInfo) 
@@ -68,25 +68,26 @@ fn VTable (comptime ClassesEnum: type, comptime Classes: var) type {
         .Struct => |data| {
             if (data.fields.len != enumInfo.Enum.fields.len) @compileError("ClassEnum and Structs are not even");
             for (data.fields) |field| 
-                if (!@hasField(ClassesEnum, field.name)) 
+                if (!@hasField(TagType, field.name)) 
                     @compileError("ClassEnum and Structs are not even");
         },
         else => @compileError("Not a struct")
     }
     
     return struct {
-
+        pub const Tag = TagType;
         fn ReturnType(comptime funcName: []const u8) type {
-            return @typeInfo(@TypeOf(@field(@field(Classes, classesInfo.Struct.fields[0].name), funcName))).Fn.return_type.?;
+            return @typeInfo(@TypeOf(@field(@field(Map, classesInfo.Struct.fields[0].name), funcName))).Fn.return_type.?;
         }
         fn FunctionType(comptime funcName: []const u8) type {
-            return @TypeOf(@field(@field(Classes, classesInfo.Struct.fields[0].name), funcName));
+            return @TypeOf(@field(@field(Map, classesInfo.Struct.fields[0].name), funcName));
         }
         pub fn Fn(comptime funcName: []const u8) (fn(var, var) ReturnType(funcName)) {
             
             const fnArray = br: {
                 var _fnArray: [classesInfo.Struct.fields.len] FunctionType(funcName) = undefined;
-                for (classesInfo.Struct.fields) |field| _fnArray[@enumToInt(@field(ClassesEnum, field.name))] = @field(@field(Classes, field.name),funcName);
+                for (classesInfo.Struct.fields) |field|
+                    _fnArray[@enumToInt(@field(TagType, field.name))] = @field(@field(Map, field.name),funcName);
                 break :br &_fnArray;
             };
 
@@ -98,11 +99,4 @@ fn VTable (comptime ClassesEnum: type, comptime Classes: var) type {
             }._func;
         }
     };
-}
-
-fn enumValue(comptime Enum: type, fieldName: []const u8) @TagType(Enum) {
-    for(@typeInfo(Enum).Enum.data.fields) |field| {
-        if (std.mem.compare(u8, field.name, fieldName, fieldName.len)) return @field(Enum, field.name);
-    }
-    @compileError("Not a field.");
 }
